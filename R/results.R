be2c13d1fd27a1664da853b5b1b045de4a9112be
BenJCross1995 <92743455+BenJCross1995@users.noreply.github.com>
2024-01-21 18:46:00 +0000

    Fix result df function in results.R
    The function wasn't summarising correctly so moved the grouping/mutating higher in the loop

--- R/results.R
+++ R/results.R
@@ -19,35 +19,6 @@ result_threshold_df <- function(data, start_threshold = 0, end_threshold = 1, in
   # Create sequence of threshold values
   threshold_vec <- seq(start_threshold, end_threshold, increment)
 
-  result_with_threshold <- data.frame()
-
-  # Loop through the threshold values and bind the tables together
-  for(thresh in threshold_vec){
-
-    # If unanswered is an option then if score equal to threshold considered unanswered else >=
-    if(unanswered_if_equal == TRUE){
-      result <- data |>
-        dplyr::mutate(threshold = thresh,
-                      prediction = dplyr::case_when(
-                        threshold < score ~ 1,
-                        threshold == score ~ 0.5,
-                        threshold > score ~ 0,
-                        TRUE ~ NA_real_))
-    } else {
-      result <- data |>
-        dplyr::mutate(threshold = thresh,
-                      prediction = dplyr::case_when(
-                        threshold <= score ~ 1,
-                        threshold > score ~ 0,
-                        TRUE ~ NA_real_))
-    }
-    # Bind the results together
-    result_with_threshold <- rbind(result_with_threshold, result)
-  }
-
-  # Convert to logical for calcs
-  result_with_threshold$same_author <- as.logical(result_with_threshold$same_author)
-
   # Specific PAN metrics
   if(length(metrics) == 1 && tolower(metrics) == "pan"){
     metrics <- c('precision', 'recall', 'F1')
@@ -56,15 +27,51 @@ result_threshold_df <- function(data, start_threshold = 0, end_threshold = 1, in
   # Return the related functions
   metrics_funs <- lapply(metrics, match.fun)
 
-  # Summarise based on prediction and same_author columns and call the resulting columns
-  # by the name of the functions in metrics
-  summarised_results <- result_with_threshold |>
-    dplyr::group_by_("threshold") |>
-    dplyr::do({
-      metrics_results <- sapply(metrics_funs, function(f) f(result_with_threshold$prediction,
-                                                            result_with_threshold$same_author))
-      stats::setNames(data.frame(t(metrics_results)), metrics)
-    })
+  result_with_threshold <- data.frame()
 
-  return(summarised_results)
+  # Loop through the threshold values and bind the tables together
+  for(thresh in threshold_vec){
+
+    # If unanswered is an option then if score equal to threshold considered unanswered else >=
+    if(unanswered_if_equal == TRUE){
+
+      # Convert the score depending on if the score is greater than the threshold etc.
+      result <- data |>
+        dplyr::mutate(threshold = thresh,
+                      prediction = dplyr::case_when(
+                        threshold < score ~ 1,
+                        threshold == score ~ 0.5,
+                        threshold > score ~ 0,
+                        TRUE ~ NA_real_))
+
+
+    } else {
+
+      # If the user doesn't want the uncertain predictions measured.
+      result <- data |>
+        dplyr::mutate(threshold = thresh,
+                      prediction = dplyr::case_when(
+                        threshold <= score ~ 1,
+                        threshold > score ~ 0,
+                        TRUE ~ NA_real_))
+
+    }
+
+    # Convert to a logical value
+    result$same_author <- as.logical(result$same_author)
+
+    # Calculate the user-selected metrics based on prediction and same author
+    result <- result |>
+      dplyr::group_by(result$threshold) |>
+      dplyr::do({
+        metrics_results <- sapply(metrics_funs, function(f) f(result$prediction, result$same_author))
+        stats::setNames(data.frame(t(metrics_results)), metrics)
+      }) |>
+      dplyr::rename('threshold' = 'result$threshold')
+
+    # Bind the results together
+    result_with_threshold <- rbind(result_with_threshold, result)
+  }
+
+  return(result_with_threshold)
 }
